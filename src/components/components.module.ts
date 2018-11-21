a8239040fd4d53293832f1afc17729e90e712f29
soninirbhay123 <32388224+soninirbhay123@users.noreply.github.com>
2018-11-21 14:01:11 +0530

    Raj

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CartInfoComponent } from './cart-info/cart-info';
+@NgModule({
+	declarations: [CartInfoComponent],
+	imports: [],
+	exports: [CartInfoComponent]
+})
+export class ComponentsModule {}
